fe1596ba0e8eda0b4fc5e29f6b371b04a5ab8851
fatamah <fatiabbasali2018@gmail.com>
2022-03-06 17:31:29 +0300

    zahra55

--- text/text/ContentView.swift
+++ text/text/ContentView.swift
@@ -9,11 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack{
-        Text("Hello, fati")
-            .padding()
-        Text(" ios")
-        }
+        
+        VStack {
+            VStack{
+            Text("Hello, fati")
+                .padding()
+            Text(" ios")
+            }
+        
         HStack{
             Image("to1")
                     .resizable()
@@ -22,6 +25,7 @@ struct ContentView: View {
                     .resizable()
                     .frame(width: 130, height: 130)
             }
+        Text("tom and jerry")
         HStack{
             Image("tom1")
                 .resizable()
@@ -32,9 +36,11 @@ Image("mm")
 Image("hi")
                 .resizable()
                 .frame(width: 130, height: 130)
+           
+        }
+        Text("friends")
         }
     }}
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
